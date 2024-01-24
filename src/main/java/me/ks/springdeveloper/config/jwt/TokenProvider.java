96ecb40c403a4b0c903ed69e63cd297e907ced7f
최광수 <rccks0116@gmail.com>
2024-01-24 18:00:40 +0900

    jwt토큰 생성 검증

---
+++ src/main/java/me/ks/springdeveloper/config/jwt/TokenProvider.java
@@ -0,0 +1,82 @@
+package me.ks.springdeveloper.config.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import me.ks.springdeveloper.domain.User;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Set;
+
+@RequiredArgsConstructor
+@Service
+public class TokenProvider {
+    private final JwtProperties jwtProperties;
+
+    public String generateToken(User user, Duration expiredAt) {
+        Date now = new Date();
+        return makeToken(new Date(now.getTime() + expiredAt.toMillis()), user);
+    }
+
+    // jwt 토큰 만들기
+    private String makeToken(Date expiry, User user) {
+        Date now = new Date();
+
+        return Jwts.builder()
+                .setHeaderParam(Header.TYPE, Header.JWT_TYPE) // typ : jwt
+                .setIssuer(jwtProperties.getIssuer()) // iss : 발급자 - properties에서 설정한 값
+                .setIssuedAt(now)                     // iat : 발급일자
+                .setExpiration(expiry)                // exp : 만료일자
+                .setSubject(user.getEmail())          // sub : 토큰제목 - 유저 이메일
+                .claim("id", user.getId())
+                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
+                .compact();
+    }
+
+    // jwt 토큰 검증
+    public boolean validToken(String token) {
+        try {
+            Jwts.parser()
+                    .setSigningKey(jwtProperties.getSecretKey())
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    // jwt 토큰으로 인증정보 가져오기
+    public Authentication getAuthentication(String token) {
+        Claims claims = getClaims(token);
+        Set<SimpleGrantedAuthority> authorities = Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+        return new UsernamePasswordAuthenticationToken(
+                new org.springframework.security.core.userdetails.User(
+                        claims.getSubject()
+                        , ""
+                        , authorities)
+                , token
+                , authorities);
+    }
+
+    public Long getUserId(String token) {
+        Claims claims = getClaims(token);
+        return claims.get("id", Long.class);
+
+    }
+    private Claims getClaims(String token){
+        return Jwts.parser()
+                .setSigningKey(jwtProperties.getSecretKey())
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+
+}
---
+++ src/test/java/me/ks/springdeveloper/config/jwt/TokenProviderTest.java
@@ -0,0 +1,85 @@
+package me.ks.springdeveloper.config.jwt;
+
+import io.jsonwebtoken.Jwts;
+import lombok.extern.log4j.Log4j2;
+import me.ks.springdeveloper.domain.User;
+import me.ks.springdeveloper.repository.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.Duration;
+import java.util.Date;
+
+@Log4j2
+@SpringBootTest
+public class TokenProviderTest {
+    @Autowired
+    private TokenProvider tokenProvider;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private JwtProperties jwtProperties;
+
+    @DisplayName("generateToken() : 토큰생성")
+    @Test
+    void generateToken() {
+        User testUser = userRepository.save(User.builder()
+                .email("user@gmail.com")
+                .password("user")
+                .build());
+
+        String token = tokenProvider.generateToken(testUser, Duration.ofDays(14));
+
+        Long userId = Jwts.parser()
+                .setSigningKey(jwtProperties.getSecretKey())
+                .parseClaimsJws(token)
+                .getBody()
+                .get("id", Long.class);
+
+        Assertions.assertThat(userId).isEqualTo(testUser.getId());
+    }
+
+    @DisplayName("validToken() : 토큰 검증 실패")
+    @Test
+    void validToken_invalid() {
+        String token = JwtFactory.builder()
+                .expiration(new Date(new Date().getTime() - Duration.ofDays(7).toMillis()))
+                .build()
+                .createToken(jwtProperties);
+
+        log.info(token);
+        boolean result = tokenProvider.validToken(token);
+
+        Assertions.assertThat(result).isFalse();
+    }
+
+    @DisplayName("validToken() : 토큰 검증 성공")
+    @Test
+    void validToken_valid() {
+        String token = JwtFactory.withDefaultValues().createToken(jwtProperties);
+        log.info(token);
+
+        boolean result = tokenProvider.validToken(token);
+
+        Assertions.assertThat(result).isTrue();
+    }
+
+    @DisplayName("getAuthentication() : 토큰으로 인증정보 가져오기")
+    @Test
+    void getAuthentication() {
+        String userEmail = "user@gmail.com";
+        String token = JwtFactory.builder()
+                .subject(userEmail)
+                .build()
+                .createToken(jwtProperties);
+
+        Authentication authentication = tokenProvider.getAuthentication(token);
+
+        Assertions.assertThat(((UserDetails) (authentication.getPrincipal())).getUsername()).isEqualTo(userEmail);
+    }
+}
